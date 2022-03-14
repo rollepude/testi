9e06c983cf2c7c649fb2a55e4c62e5766111f0f3
Robert Juhana Pudas <s1puro00@students.osao.fi>
2022-03-14 13:26:36 +0200

    Add project files.

---
+++ testi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
